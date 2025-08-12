823a6052b8ba3a5a680f7b40dc3e1291bd913a27
al-furkan <furkanf4336@gmail.com>
2025-08-12 23:53:12 +0600

    finish Admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -2,6 +2,8 @@ import UserCard from "@/components/UserCard";
 import CountChart from "@/components/CountChart";
 import AttendanceChart from "@/components/AttendanceChart";
 import FinanceChart from "@/components/FinanceChart";
+import EventCalendar from "@/components/EventCalendar";
+import Announcements from "@/components/Announcements";
 const Adminpage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
@@ -32,8 +34,8 @@ const Adminpage = () => {
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        {/* <EventCalendar />
-        <Announcements/> */}
+        <EventCalendar />
+        <Announcements/>
       </div>
     </div>
   );
