9005066440b2786e96b56af184592b87c0ca5b3e
al-furkan <furkanf4336@gmail.com>
2025-09-04 13:32:43 +0600

    y

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -115,7 +115,7 @@ const TeacherListPage = () => {
               // <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               //   <Image src="/plus.png" alt="" width={14} height={14} />
               // </button>
-              <FormModal table="teacher" type="create" className="text-black"/>
+              <FormModal table="teacher" type="create" />
             )}
           </div>
         </div>
